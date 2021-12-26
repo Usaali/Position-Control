e7fff01e063367a21051c466a6c08e7f94cda6db
Usama Ali <usama.ali@robotics-erlangen.de>
2021-12-26 17:33:46 +0100

    Kalman Filter works now. Tested with noisy data. Estimates all biases

--- INS.m
+++ INS.m
@@ -1,20 +1,19 @@
 close all;
 clear all;
-%% Create IMU Sensor object
-% Usage: [measuredAcceleration, measuredAngularVelocity] = IMU(acceleration, angularVelocity)
-%accparams = accelparams('MeasurementRange',19.6,'Resolution',0.598e-3,'ConstantBias',0.49,'NoiseDensity',3920e-6,'TemperatureBias',0.294,'TemperatureScaleFactor',0.02,'AxesMisalignment',2);
-%IMU = imuSensor('Accelerometer', accparams); % realistic imu sensor
-imuFs = 100;
-IMU = imuSensor('SampleRate',imuFs); % ideal imu sensor
+
 %% Create a dummy trajectory
- wp = [0,0,0;10,0,0;10,10,0;0,10,0;0,0,0]; % Waypoints
- traj = waypointTrajectory(wp,0:4,'SampleRate',100);
+ %wp = [0,0,0;10,0,0;13,10,0;0,10,0]; % Waypoints
+ %wp = [wp;wp;wp;wp;wp;wp;wp;0,0,0 ];
+ frequency = 300;
+ wp = [0,0,0;0,5,0;5,5,0;13,7,0;15,15,0;12,15,0;12,11,0;16,11,0;16,8,0;4,8,0;4,20,0;0,20,0;3,10,0;0,0,0]
+ traj = waypointTrajectory(wp,0:size(wp,1)-1,'SampleRate',frequency);
  
  %% Get data out of trajectory
  traj.reset()
  [pos,orient,vel,acc,angVel] = traj.step();
  ang = quat2eul(orient);
  cnt = 1;
+ piCnt = 0;
  spf = traj.SamplesPerFrame;
  while ~isDone(traj)
      idx = (cnt+1):(cnt+spf);
@@ -22,114 +21,108 @@ IMU = imuSensor('SampleRate',imuFs); % ideal imu sensor
      ang(idx,:) = quat2eul(orient(idx,:));
      cnt = cnt+1;
  end
- 
- 
-%% Plot trajectory and points
-figure('Name','Groundtruth Position', 'NumberTitle','off');
-plot(pos(:,1),pos(:,2), wp(:,1),wp(:,2), '--o')
-title('Position')
-xlabel('X (m)')
-ylabel('Y (m)')
-legend({'Position', 'Waypoints'})
-axis equal
-figure('Name','Groundtruth Acceleration', 'NumberTitle','off');
-subplot(3,1,1); 
-plot(acc(:,1))
-title('X-Acceleration')
-xlabel('Time')
-ylabel('Acc (m/s^2)')
-subplot(3,1,2);
-plot(acc(:,2))
-title('Y-Acceleration')
-xlabel('Time')
-ylabel('Acc (m/s^2)')
-subplot(3,1,3);
-plot(acc(:,3))
-title('Z-Acceleration')
-xlabel('Time')
-ylabel('Acc (m/s^2)')
 
-figure('Name','Groundtruth Speed', 'NumberTitle','off');
-subplot(3,1,1); 
-plot(vel(:,1))
-title('X-Speed')
-xlabel('Time')
-ylabel('Acc (m/s)')
-subplot(3,1,2);
-plot(vel(:,2))
-title('Y-Speed')
-xlabel('Time')
-ylabel('Acc (m/s)')
-subplot(3,1,3);
-plot(vel(:,3))
-title('Z-Speed')
-xlabel('Time')
-ylabel('Acc (m/s)')
+%% Create Imu data 
+accm = zeros(size(acc,1),2);
 
-%% get IMU Data
-[accm, angVelm] = IMU(acc, angVel);
-
-figure('Name','IMU Acceleration', 'NumberTitle','off');
-subplot(3,1,1); 
-plot(accm(:,1))
-title('X-Acceleration')
-xlabel('Time')
-ylabel('Acc (m/s^2)')
-subplot(3,1,2);
-plot(accm(:,2))
-title('Y-Acceleration')
-xlabel('Time')
-ylabel('Acc (m/s^2)')
-subplot(3,1,3);
-plot(accm(:,3))
-title('Z-Acceleration')
-xlabel('Time')
-ylabel('Acc (m/s^2)')
-
-%% Add gaussian noise to the position
-imuPerGlob = 1;
-
-var = 0.2;
-rPos = [normrnd(pos(1,1),var),normrnd(pos(1,2),var),normrnd(pos(1,3),var)];
-for i=2:size(pos,1)
-    if mod(i-1,imuPerGlob)==0
-        rPos(((i-1)/imuPerGlob)+1,:) = [normrnd(pos(i,1),var),normrnd(pos(i,2),var),normrnd(pos(i,3),var)];
-    end
+for i = 1:size(acc,1)
+    rotMat = [cos(ang(i,1)),sin(ang(i,1));-sin(ang(i,1)),cos(ang(i,1))];
+    temp = rotMat*[acc(i,1);acc(i,2)];
+    accm(i,:) = [temp(1), temp(2)];
 end
 
-%figure('Name','Vision Position', 'NumberTitle','off');
-%plot(rPos(:,1),rPos(:,2), wp(:,1),wp(:,2), '--o')
 
+%% Add gaussian noise to the position
+posvar = 0.2;
+posbias = 0;
+rposnoise = normrnd(0,posvar,size(pos,1),size(pos,2));
+rposnoise = rposnoise - mean(rposnoise) + posbias;
+rPos = pos + rposnoise;
+
+rangvar = 0.15;
+rangbias = 0;
+rangnoise = normrnd(0,rangvar,size(ang,1),size(ang,2));
+rangnoise = rangnoise - mean(rangnoise) + rangbias;
+rAng = ang + rangnoise;
+
+accvar = 0.01;
+accbias = 0.45;
+accnoise = normrnd(accbias,accvar,size(accm,1),size(accm,2));
+accnoise = accnoise - mean(accnoise) + accbias;
+accm = accm + accnoise;
+
+gyrvar = 0.05;
+gyrbias = 0.43;
+gyrnoise = normrnd(gyrbias,gyrvar,size(angVel,1),size(angVel,2));
+gyrnoise = gyrnoise - mean(gyrnoise) + gyrbias;
+angVelm = angVel + gyrnoise;
+
+figure('Name','Vision Position', 'NumberTitle','off');
+plot(rPos(:,1),rPos(:,2), wp(:,1),wp(:,2), '--o')
+figure('Name','Vision Angle', 'NumberTitle','off');
+plot(rAng(:,1))
+figure('Name','IMU', 'NumberTitle','off');
+subplot(3,1,1); 
+plot(accm(:,1))
+title('X-Acc')
+xlabel('Time')
+ylabel('x-acc')
+subplot(3,1,2); 
+plot(accm(:,2))
+title('Y-Acc')
+xlabel('Time')
+ylabel('y-acc')
+subplot(3,1,3);
+plot(angVelm(:,3))
+title('Measured angular velocity (yaw)')
+xlabel('Time')
+ylabel('Vel (rad/s^2)')
+
+%% run the filter
 filt = KalmanFusionFilt()
-%filt.x(1) = vel(1,1);
-%filt.x(2) = vel(1,2);
+filt.x(1) = vel(1,1);
+filt.x(2) = vel(1,2);
+filt.tx(1) = ang(1,1);
 
-% numImuSamples = size(accm,1);
-% estPos = zeros(numImuSamples,2);
-% estVel = zeros(numImuSamples,2);
-% estAng = zeros(numImuSamples,2);
-% gpsIdx = 1;
-% for idx = 1:numImuSamples
-%     if(mod(idx,imuPerGlob)==0)        
-%         filt.correct(angVel(idx,3),rPos(gpsIdx,1), rPos(gpsIdx,2),ang(gpsIdx,1))
-%         gpsIdx = gpsIdx + 1;
-%     end
-%     filt.predict(1/100,accm(idx,1),accm(idx,2));
-%     
-%     estPos(idx,:) = filt.getPos();
-%     estVel(idx,:) = filt.getVel();
-%     estAng(idx,:) = filt.getAng();
-% end
-% figure('Name','Estimated Position', 'NumberTitle','off');
-% plot(estPos(:,1),estPos(:,2), wp(:,1),wp(:,2), '--o', pos(:,1),pos(:,2),'r')
-% 
-% figure('Name','Estimated Angle', 'NumberTitle','off');
-% hold('on')
-% plot(estAng(:,2))
-% plot(ang(:,1),'r')
-% title('Theta')
-% xlabel('Time')
-% 
+numImuSamples = size(accm,1);
+imuPerGlob = 5;
+estPos = zeros(numImuSamples,2);
+estVel = zeros(numImuSamples,2);
+estAng = zeros(numImuSamples,1);
+
+for idx = 1:numImuSamples
+    
+    filt.predictTheta(1/frequency,angVelm(idx,3));
+    
+    
+    if(mod(idx,imuPerGlob)==0)
+        filt.correctTheta(rAng(idx,1));
+    end
+    filt.predict(1/frequency,accm(idx,1),accm(idx,2));
+    if(mod(idx,imuPerGlob)==0)
+        filt.correct(rPos(idx,1), rPos(idx,2))
+    end
+    estPos(idx,:) = filt.getPos();
+    estVel(idx,:) = filt.getVel();
+    estAng(idx) = filt.getAng();
+end
+figure('Name','Estimated Position', 'NumberTitle','off');
+plot(estPos(:,1),estPos(:,2), wp(:,1),wp(:,2), '--o', pos(:,1),pos(:,2),'r',rPos(:,1),rPos(:,2),'.g');
+
+figure('Name','Estimated Angle', 'NumberTitle','off');
+hold('on')
+plot(estAng)
+plot(ang(:,1),'r')
+title('Theta')
+xlabel('Time')
+figure('Name','Angle Error', 'NumberTitle','off');
+plot(estAng-ang(:,1))
+title('delta Theta')
+xlabel('Time')
+figure('Name','Position Error', 'NumberTitle','off');
+plot(sqrt((estPos(:,2)-pos(:,2)).^2 + (estPos(:,1)-pos(:,1)).^2))
+title('delta pos')
+xlabel('Time')
 % figure('Name','Angular velocity', 'NumberTitle','off');
 % subplot(2,1,1); 
 % plot(angVel(:,3))
@@ -137,11 +130,11 @@ filt = KalmanFusionFilt()
 % xlabel('Time')
 % ylabel('Vel (rad/s^2)')
 % subplot(2,1,2);
-% plot(angVelm(:,3))
+% plot(estAng(:,1))
 % title('Measured angular velocity (yaw)')
 % xlabel('Time')
 % ylabel('Vel (rad/s^2)')
-% 
+
 % intVel = cumsum(angVel(:,3))*0.01;
 % figure('Name','Angular velocity Integrated', 'NumberTitle','off');
 % plot(intVel)
@@ -149,22 +142,4 @@ filt = KalmanFusionFilt()
 % a = sqrt(vel(:,1).^2 + vel(:,2).^2);
 % figure('Name','Speed normalized', 'NumberTitle','off');
 % plot(a)
-xAcc = zeros(size(acc,1),1);
-yAcc = zeros(size(acc,1),1);
-for i = 1:size(acc,1)
-    temp = [acc(i,1),acc(i,2)]*[cos(ang(i,1)),-sin(ang(i,1));sin(ang(i,1)),cos(ang(i,1))];
-    xAcc(i) = temp(1);
-    yAcc(i) = temp(2);
-end
 
-figure('Name','Calc Acceleration', 'NumberTitle','off');
-subplot(2,1,1); 
-plot(xAcc)
-title('X-Acceleration')
-xlabel('Time')
-ylabel('Acc (m/s^2)')
-subplot(2,1,2);
-plot(yAcc)
-title('Y-Acceleration')
-xlabel('Time')
-ylabel('Acc (m/s^2)')
--- KalmanFusionFilt.m
+++ KalmanFusionFilt.m
@@ -3,10 +3,14 @@ classdef KalmanFusionFilt < handle
     %   Detailed explanation goes here
     
     properties
-        x %state vector [6,1]: [vx,vy,x,y,w,theta]
+        x %state vector [6,1]: [vx,vy,x,y,xbias,ybias]
         P %covariance matrix [6,6]
         Q %process covariance
         R %measurement covariance
+        tx % state vector for theta estimation [theta, thetabias]
+        thetaP
+        thetaQ
+        thetaR
     end
     
     methods
@@ -15,35 +19,52 @@ classdef KalmanFusionFilt < handle
             %   Detailed explanation goes here
             obj.x = zeros(6,1);
             obj.P = eye(6) * 1;
-            obj.Q = eye(6)*1e-4;
-            obj.R = eye(3)*0.002;
+            obj.Q = eye(6)*1e-5;
+            obj.R = diag([1e-2,1e-2]);
+            obj.tx = zeros(2,1); 
+            obj.thetaP = eye(2);
+            obj.thetaQ = diag([1e-4,1e-4]);
+            obj.thetaR = 1e-2;
         end
         
-        function predict(obj, T, ax, ay, theta)
-            
-            %theta = obj.x(6);
+        function predictTheta(obj,T, w)
+            A = [1,-T;0,1];
+            B = [T;0];
+            obj.tx = A*obj.tx + B*w;
+            obj.thetaP = A*obj.thetaP*A' + obj.thetaQ;
+        end
+        
+        function correctTheta(obj, senstheta)
+            %C is 1
+            if abs(senstheta - obj.tx(1)) > pi
+                obj.tx(1) = obj.tx(1) + 2*pi * sign(senstheta);
+            end
+            C = [1,0];
+            y = senstheta - C*obj.tx; %Innovation
+            S = C*obj.thetaP*C' + obj.thetaR; %innovation Cov
+            K = (obj.thetaP*C')/S; %kalman Gain
+            obj.tx = obj.tx + K*y; %state update
+            obj.thetaP = (eye(2)-K*C)*obj.thetaP; %cov update
+        end
+        
+        function predict(obj, T, ax, ay)
+            theta = obj.tx(1);
             A = eye(6);
-            A(1,6) = (-ax*sin(theta)-ay*cos(theta)) * T;
-            A(2,6) = (ax*cos(theta)-ay*sin(theta)) * T;
+            A(1,5) = -T*cos(theta);
+            A(1,6) = T*sin(theta);
+            A(2,5) = -T*sin(theta);
+            A(2,6) = -T*cos(theta);
             A(3,1) = T;
             A(4,2) = T;
-            A(6,5) = T;
-            B = [cos(theta),-sin(theta);sin(theta),cos(theta);0,0;0,0;0,0;0,0];
-            obj.x = A*obj.x + B*T*[ax,ay]';
-%             obj.x(3) = obj.x(3) + obj.x(1)*T;
-%             obj.x(4) = obj.x(4) + obj.x(2)*T;
-%             obj.x(6) = obj.x(6) + obj.x(5)*T;
-%             obj.x(1) = obj.x(1) + (ax*cos(theta)-ay*sin(theta)) * T;
-%             obj.x(2) = obj.x(2) + (ax*sin(theta)+ay*cos(theta)) * T;
-            
+            B = [cos(theta),-sin(theta);sin(theta),cos(theta);T/2,0;0,T/2;0,0;0,0];
+            obj.x = A*obj.x + B*T*[ax;ay];
             obj.P = A*obj.P*A' + obj.Q;
         end
         
-        function correct(obj, w, sensx, sensy, senstheta)
-            C = zeros(3,6);
-            C(:,3:5) = eye(3);
-           % C(6,6)=0;
-            y = [sensx, sensy, w]' - C*obj.x; %Innovation
+        function correct(obj, sensx, sensy)
+            C = zeros(2,6);
+            C(:,3:4) = eye(2);
+            y = [sensx, sensy]' - C*obj.x; %Innovation
             S = C*obj.P*C' + obj.R; %innovation Cov
             K = (obj.P*C')/S; %kalman Gain
             obj.x = obj.x + K*y; %state update
@@ -59,7 +80,7 @@ classdef KalmanFusionFilt < handle
         end
         
         function ang = getAng(obj)
-            ang = [obj.x(5);obj.x(6)];
+            ang = obj.tx(1);
         end
         
     end
